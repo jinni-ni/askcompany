19537d8c637b4c793a1e0d6dc024e15e01546013
김수진 <sjkim910115@gmail.com>
2020-10-25 21:21:20 +0900

    비밀번호 수정

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm,\
+    PasswordChangeForm as AuthPasswordChangeForm
 from .models import User
 
 class SignupForm(UserCreationForm):
@@ -27,3 +28,16 @@ class ProfileForm(forms.ModelForm):
         model = User
         fields = ['avatar', 'first_name', 'last_name', 'website_url',
                   'bio', 'phone_number', 'gender']
+
+class PasswordChangeForm(AuthPasswordChangeForm):
+    # 재정의
+    def clean_new_password2(self):
+        old_password = self.cleaned_data.get('old_password')
+        new_password1 = self.cleaned_data.get('new_password1')
+        new_password2 = super().clean_new_password2()
+        if old_password and new_password1:
+            if old_password == new_password2:
+                raise forms.ValidationError("새로운 암호는 기존 암호와 다르게 입력해주세요.")
+        return new_password2
+
+
